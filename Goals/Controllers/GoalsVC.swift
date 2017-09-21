d0f613d9cfadf3ebdeff6f54dd8655f37c0218cf
Steve Baker <stevebaker@Steves-MBP-2.gateway>
2017-09-21 19:41:36 +1000

    Initial commit

---
+++ Goals/Controllers/GoalsVC.swift
@@ -0,0 +1,24 @@
+//
+//  GoalsVC.swift
+//  Goals
+//
+//  Created by Steve Baker on 21/9/17.
+//  Copyright © 2017 Steve Baker. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var goalTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addGoalPressed(_ sender: Any) {
+    }
+    
+}
--- Goals/Controllers/ViewController.swift
+++ Goals/Controllers/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Goals.swift
 //  Goals
 //
 //  Created by Steve Baker on 21/9/17.
@@ -8,16 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
