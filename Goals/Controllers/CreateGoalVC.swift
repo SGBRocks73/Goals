6b4ddb20c412472691475692fc4cf1a857855c96
Steve Baker <stevebaker@Steves-MBP-2.gateway>
2017-09-22 14:09:37 +1000

    Updated keyboard UI

--- Goals/Controllers/CreateGoalVC.swift
+++ Goals/Controllers/CreateGoalVC.swift
@@ -17,11 +17,14 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
-    
+    var goalType: GoalType = .shortTerm
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColour()
+        longTermButton.setDeselectedColour()
         // Do any additional setup after loading the view.
     }
     
@@ -30,10 +33,16 @@ class CreateGoalVC: UIViewController {
     }
     
     @IBAction func longTermButtonPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermButton.setSelectedColour()
+        shortTermButton.setDeselectedColour()
     }
     
    
     @IBAction func shortTermButtonPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermButton.setSelectedColour()
+        longTermButton.setDeselectedColour()
     }
     
     @IBAction func nextButtonPressed(_ sender: Any) {
---
+++ Goals/Extensions/UIButtonExt.swift
@@ -0,0 +1,20 @@
+//
+//  UIButtonExt.swift
+//  Goals
+//
+//  Created by Steve Baker on 22/9/17.
+//  Copyright © 2017 Steve Baker. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setSelectedColour() {
+        self.backgroundColor = #colorLiteral(red: 0.09360925108, green: 0.8409675956, blue: 0.4633229375, alpha: 1)
+    }
+    
+    func setDeselectedColour() {
+        self.backgroundColor = #colorLiteral(red: 0.7464011312, green: 0.8857318759, blue: 0.7402122021, alpha: 1)
+    }
+}
